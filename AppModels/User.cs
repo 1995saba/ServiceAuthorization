35aec893bb39381ebadb835cfc11100403883abb
Sabina <sabina.orunbaeva@gmail.com>
2017-10-17 18:19:09 +0600

    Add files via upload

---
+++ AppModels/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppModels
+{
+    public class User
+    {
+        private int id=0;
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string OtherInfo { get; set; }
+
+        public User()
+        {
+            id++;
+        }
+    }
+}
