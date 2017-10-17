3aca6771887ff82b755086280f4fbd2161ec3ec3
Sabina <sabina.orunbaeva@gmail.com>
2017-10-17 18:24:40 +0600

    Add files via upload

---
+++ AppServices/Authorization.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppModels;
+using AppData;
+
+namespace AppServices
+{
+    public class Authorization
+    {
+        public User currentUser = new User();
+        DataBase dataBase = new DataBase();
+
+        public User AuthorityVerify(string login, string password)
+        {
+            foreach (var user in dataBase.GetUsers())
+            {
+                if (user.Login == login && user.Password == password)
+                {
+                    return user;
+                }
+            }
+            return null;
+        }
+
+    }
+}
